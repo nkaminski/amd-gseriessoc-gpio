bd59d10a7d271bb5001cef3ca2bbf5ee169bb83b
Nash Kaminski <nashkaminski@comcast.net>
2015-01-23 00:01:16 -0600

    Format log entries correctly

--- gpio-amd.c
+++ gpio-amd.c
@@ -27,6 +27,7 @@
 *    
 *   
 ***************************************************************************/
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/pci.h>
